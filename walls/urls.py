6b36ffca5b3d18d768010738ba14db5ad5f66a62
unknown <msqui@.(none)>
2011-07-15 14:41:48 +0400

    Add Wall view now uses login-required generic CreateView

--- walls/urls.py
+++ walls/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls.defaults import patterns, url
+from django.contrib.auth.decorators import login_required
+from walls.views import AddView
 
 urlpatterns = patterns('',
     url(r'^$', 'walls.views.index', name='walls_index'),
-    url(r'^add/$', 'walls.views.add', name='walls_add'),
+
+    url(r'^add/$', login_required(AddView.as_view()), name='walls_add'),
+
     url(r'^(?P<wall_id>\w+)/$', 'walls.views.show', name='walls_show'),
 )
