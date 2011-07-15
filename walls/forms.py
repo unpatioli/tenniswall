f061007d1194a321336a573ecd00fa27f5f79e91
unknown <msqui@.(none)>
2011-07-15 14:39:42 +0400

    custom form replaced with ModelForm

--- walls/forms.py
+++ walls/forms.py
@@ -1,23 +1,16 @@
-from django import forms
+from django.contrib.gis import forms as geoforms
+from mapper.widgets import GoogleMapPickLocationWidget
+from walls.models import FreeWall, PaidWall
 
-class AddWallForm(forms.Form):
-    title = forms.CharField(max_length=100, required=False)
-    address = forms.CharField(max_length=500)
-    lat = forms.FloatField()
-    lon = forms.FloatField()
+class LocationWidgets:
+    widgets = {
+        'location': GoogleMapPickLocationWidget()
+    }
 
-    def clean(self):
-        """
-        Changes form's cleaned_data to contain 'location' dict instead of
-        lat and lon attributes
-        """
-        lat = self.cleaned_data.pop('lat')
-        lon = self.cleaned_data.pop('lon')
+class FreeWallForm(geoforms.ModelForm):
+    class Meta(LocationWidgets):
+        model = FreeWall
 
-        self.cleaned_data['location'] = {
-            'lat': lat,
-            'lon': lon,
-        }
-        return self.cleaned_data
-
-
+class PaidWallForm(geoforms.ModelForm):
+    class Meta(LocationWidgets):
+        model = PaidWall
