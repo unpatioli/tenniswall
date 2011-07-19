b86934d7a53a0014f723cfb9e5407f0f89db6698
msqui <msqui@yandex.ru>
2011-07-19 16:03:19 +0400

    walls urls added

--- walls/urls.py
+++ walls/urls.py
@@ -1,9 +1,27 @@
 from django.conf.urls.defaults import patterns, url
 from django.contrib.auth.decorators import login_required
-from walls.views import AddView
+from walls.views import AddPaidView, AddFreeView
 
 urlpatterns = patterns('',
-    url(r'^$', 'walls.views.index', name='walls_index'),
+    url(r'^$',
+        'walls.views.index',
+        name='walls_index'
+    ),
+    url(r'paid/$',
+        'walls.views.index',
+        name='walls_paid'
+    ),
+    url(r'free/$',
+        'walls.views.index',
+        name='walls_free'
+    ),
 
-    url(r'^add/$', login_required(AddView.as_view()), name='walls_add'),
+    url(r'^paid/add/$',
+        login_required(AddPaidView.as_view()),
+        name='walls_paid_add'
+    ),
+    url(r'^free/add/$',
+        login_required(AddFreeView.as_view()),
+        name='walls_free_add'
+    )
 )
