1c417cfbcaec8de9e7b9595f1fe4843b6e721369
msqui <msqui@yandex.ru>
2011-07-23 21:24:42 +0400

    unused signal receiver removed

--- walls/models/util.py
+++ walls/models/util.py
@@ -1,6 +1,4 @@
 from django.db import models
-from django.db.models.signals import pre_save
-from django.dispatch.dispatcher import receiver
 
 class Timestamps(models.Model):
     created_at = models.DateTimeField(auto_now_add=True,
@@ -25,11 +23,6 @@ class Paranoid(models.Model):
 
         self.deleted_at = datetime.now()
         self.save()
-
-@receiver(pre_save, sender=Paranoid, dispatch_uid="Paranoid_connector")
-def paranoid(sender, **kwargs):
-    from datetime import datetime
-    sender.deleted_at = datetime.now()
 
 class Ban(models.Model):
     approved_at = models.DateTimeField(null=True, blank=True,
