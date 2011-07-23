3cc69dadce5fe6fbd51c53659e934000ffc802a7
msqui <msqui@yandex.ru>
2011-07-23 22:17:33 +0400

    honeypot field added and used in comments system

--- walls/forms.py
+++ walls/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.gis import forms as geoforms
+from django.utils.translation import ugettext as _
 from mapper.widgets import GoogleMapPickLocationWidget
 from walls.models import Wall, WallComment
 
@@ -12,6 +13,17 @@ class WallForm(geoforms.ModelForm):
         }
 
 class WallCommentForm(forms.ModelForm):
+    honeypot = forms.CharField(required=False,
+                               label=_('If you enter anything in this field '\
+                                       'your comment will be treated as spam'))
+
+    def clean_honeypot(self):
+        """Check that nothing's been entered into the honeypot."""
+        value = self.cleaned_data["honeypot"]
+        if value:
+            raise forms.ValidationError(self.fields["honeypot"].label)
+        return value
+
     class Meta:
         model = WallComment
 
