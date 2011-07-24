c8f341e711d1eae8c96cca2410d9b2c6be3a6b1e
msqui <msqui@yandex.ru>
2011-07-24 12:20:36 +0400

    front-page map requests markers and shows them

--- static_files/js/front_page_map.js
+++ static_files/js/front_page_map.js
@@ -12,4 +12,47 @@ $(function(){
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     var map = new google.maps.Map(document.getElementById(map_id), myOptions);
+    var markers = [];
+    marker_threshold = 10;
+    google.maps.event.addListener(map, 'idle', function(event){
+        var bounds = map.getBounds();
+        var ne = bounds.getNorthEast();
+        var sw = bounds.getSouthWest();
+
+        var json = $.toJSON({
+            ne: {
+                lat: ne.lat(),
+                lng: ne.lng()
+            },
+            sw: {
+                lat: sw.lat(),
+                lng: sw.lng()
+            },
+            num: 5
+        });
+
+        $.ajax({
+            type: "POST",
+            url: "walls/bbox.json/",
+            data: json,
+            dataType: "json",
+            contentType: "application/json; charset=utf-8",
+            success: function(data){
+                if (markers.length > marker_threshold) {
+                    $.each(markers, function(key, val){
+                        val.setMap(null);
+                        val = null;
+                    });
+                    markers = [];
+                }
+                $.each(data, function(key, value){
+                    markers.push(new google.maps.Marker({
+                        map: map,
+                        position: new google.maps.LatLng(value.lat, value.lng),
+                        draggable: false
+                    }));
+                });
+            }
+        });
+    });
 }); 
