1a4c9c1e464ba3fa4fad97c0b7d6c1005618120a
msqui <msqui@yandex.ru>
2011-07-25 10:03:14 +0400

    Fluster2 plugin added to cluster GMap markers

--- static_files/js/front_page_map.js
+++ static_files/js/front_page_map.js
@@ -5,16 +5,44 @@ $(function(){
     });
     $("#content").append(map_div);
 
+    var debug = $('<div></div>')
+    $("#content").append(debug);
+
     var latlng = new google.maps.LatLng(50, 16);
     var myOptions = {
         zoom: 8,
         center: latlng,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-    var map = new google.maps.Map(document.getElementById(map_id), myOptions);
+    var map = new google.maps.Map(map_div.get(0), myOptions);
     var markers = [];
-    max_markers_count = 10;
-    marker_threshold = max_markers_count * 3;
+    var max_markers_count = 10;
+    var marker_threshold = max_markers_count * 3;
+    var fluster = new Fluster2(map);
+
+    function checkMarker(coord) {
+        for (var i = 0; i < fluster.markers.length; ++i) {
+            var val = fluster.markers[i];
+            if (coord.lat == val.position.lat() &&
+                    coord.lng == val.position.lng()) {
+                return true;
+            }
+        };
+        return false;
+    }
+
+    function clearMarkers(bbox) {
+        for (var i = 0; i < fluster.markers.length; ++i) {
+            var val = fluster.markers[i];
+            if (!bbox.contains(val.position)) {
+                fluster.markers[i].setMap(null);
+                fluster.markers[i] = null;
+                fluster.markers.splice(i,1);
+                i--;
+            }
+        }
+    }
+
     google.maps.event.addListener(map, 'idle', function(event){
         var bounds = map.getBounds();
         var ne = bounds.getNorthEast();
@@ -32,6 +60,12 @@ $(function(){
             num: max_markers_count
         });
 
+        clearMarkers(bounds);
+
+        // DEBUG
+        debug.html("Count: " + fluster.markers.length);
+        // END DEBUG
+
         $.ajax({
             type: "POST",
             url: "walls/bbox.json/",
@@ -39,29 +73,36 @@ $(function(){
             dataType: "json",
             contentType: "application/json; charset=utf-8",
             success: function(data){
-                if (markers.length > marker_threshold) {
-                    $.each(markers, function(key, val){
-                        val[0].setMap(null);
-                        val = null;
-                    });
-                    markers = [];
-                }
+//                if (markers.length > marker_threshold) {
+//                    $.each(markers, function(key, val){
+//                        val[0].setMap(null);
+//                        val[0] = null;
+//                        val[1] = null;
+//                    });
+//                    markers = [];
+//                }
+                
                 $.each(data, function(key, value){
-                    var m = new google.maps.Marker({
-                        map: map,
-                        title: value.title,
-                        position: new google.maps.LatLng(value.lat, value.lng),
-                        draggable: false
-                    });
-                    var i = new google.maps.InfoWindow({
-                        content: value.info
-                    });
-                    google.maps.event.addListener(m, 'click', function(event){
-                        i.open(map, m);
-                    });
-                    markers.push([m, i]);
+                    if (!checkMarker(value)) {
+                        var p = new google.maps.LatLng(value.lat, value.lng);
+                        var m = new google.maps.Marker({
+    //                        map: map,
+                            title: value.title,
+                            position: p,
+                            draggable: false
+                        });
+                        var i = new google.maps.InfoWindow({
+                            content: value.info
+                        });
+                        google.maps.event.addListener(m, 'click', function(event){
+                            i.open(map, m);
+                        });
+                        fluster.addMarker(m);
+//                        markers.push([m, i]);
+                    }
                 });
             }
         });
     });
+    fluster.initialize();
 }); 
